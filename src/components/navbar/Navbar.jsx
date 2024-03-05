a9ffe29b7d80da433f284f3bce8fc881e33e1e13
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-03-05 23:06:13 +0200

    finished hero section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,27 +7,28 @@ const Navbar = () => {
       {/* SIDEBAR  */}
       <Sidebar />
       <div className="wrapper">
-        <motion.span
+        <motion.a
+          href="/"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
           HASSAN TECH
-        </motion.span>
+        </motion.a>
         <div className="social">
-          <a href="#">
+          <a href="https://www.facebook.com/hassan.hammoud.7127/">
             <img src="/facebook.png" alt="" />
           </a>
 
-          <a href="#">
+          <a href="https://www.instagram.com/hssn_3ha/">
             <img src="/instagram.png" alt="" />
           </a>
 
-          <a href="#">
+          <a href="https://github.com/Hassan-Hammoud">
             <img src="/dribble.png" alt="" />
           </a>
 
-          <a href="#">
+          <a href="https://www.linkedin.com/in/hassan-hammoud-3ha/">
             <img src="/youtube.png" alt="" />
           </a>
         </div>
