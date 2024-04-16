9de600a9e6b2281ce3c23e7165d3764886aafc34
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-04-16 23:47:47 +0300

    add my projects link

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,27 +5,24 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "React Commerce",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "Admin Dashboard",
+    img: "https://i.ibb.co/Mh81Ms3/GB-dark-Dashboard.jpg",
+    desc: "An advanced management dashboard developed using the MERN stack. The backend is built using powerful and secure technologies such as Node.js, Express.js, Mongoose, and MongoDB, enabling efficient data modeling and retrieval. Advanced tools like Material UI, Nivo Charts and Redux Toolkit have been used in the front-end.",
+    link: "https://ad-dashboard-one.vercel.app/dashboard",
   },
   {
     id: 2,
-    title: "Next.js Blog",
-    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "Monueh Shop",
+    img: "https://i.ibb.co/nDYYdT7/Browse-thousands-of-Landing-Food-Delivery-images-for-design-inspiration.jpg",
+    desc: "This is the final project at which is an e-commerce website showcasing classic and quality products Products taken by mountain village people, advanced Customers for safe and child-safe shopping.",
+    link: "https://monuehshop.vercel.app/",
   },
   {
     id: 3,
-    title: "Vanilla JS App",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-  },
-  {
-    id: 4,
-    title: "Music App",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "Hssn.Tech Project",
+    img: "https://i.ibb.co/C2S8XGp/ODESZA-promo-website-redesign-concept-UI-UX.jpg",
+    desc: "A Portfolio project with the guidance and inspiration provided by the Al Zero Web School channel. What started as a source of knowledge and ideas soon transformed into a canvas for innovation and a testament to my commitment to continuous learning.",
+    link: "https://template-three.onrender.com/#",
   },
 ];
 
@@ -48,7 +45,9 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <a href={item.link}>
+              <button>See Demo</button>
+            </a>
           </motion.div>
         </div>
       </div>
